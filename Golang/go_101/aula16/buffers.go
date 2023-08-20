235d30aa8ebf52a7d7b6b558181bbe61042d2c0e
passon <kayllonsv.passon@gmail.com>
2023-08-20 13:58:29 -0300

    Golang: Otimizando channels com buffer

---
+++ Golang/go_101/aula16/buffers.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func numeros(n chan <- int) {
+	for i := 0; i < 10; i++ {
+		n <- i
+		fmt.Printf("Escrito no channel: %d\n", i)
+		time.Sleep(time.Millisecond * 90)
+	}
+	fmt.Println("Fim da escrita")
+	close(n)
+}
+
+func main() {
+	cn := make(chan int, 3)
+	go numeros(cn)
+
+	for v := range cn {
+		fmt.Printf("Lido do channel: %d\n", v)
+		time.Sleep(time.Millisecond * 180)
+	}
+	fmt.Println("Fim da execução!")
+}
